5ac141ad563eb31ef4ad77e0f3c5e93cc369818f
Olexander Khrystychenko <alexander.khristichenko@gmail.com>
2023-10-29 10:51:15 +0200

    added popup and logic subscribe form in footer

--- src/components/Footer/FooterContact.jsx
+++ src/components/Footer/FooterContact.jsx
@@ -1,9 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./FooterContact.module.scss";
 import { Input } from "../UI/Input";
 import { Button } from "../UI/Button";
+import { useDispatch } from "react-redux";
+import { showMsg, toggle } from "@/store/popup-slice";
+import { useState } from "react";
 
 export const FooterContact = () => {
+  const dispatch = useDispatch();
+  const [enteredEmail, setEnteredEmail] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredEmail.trim().includes("@")) {
+      dispatch(
+        showMsg({ msg: `Thanks for your subscription ${enteredEmail}` }),
+      );
+      dispatch(toggle({ isVisiblePopup: true }));
+    }
+
+    setEnteredEmail("");
+  };
+
   return (
     <div className={styles.contact}>
       <h2 className={styles.title}>Contact Us</h2>
@@ -21,8 +42,13 @@ export const FooterContact = () => {
         />
         ouremailaddress@email.com
       </a>
-      <form className={styles.form}>
-        <Input type="email" placeholder="Subscribe by email" />
+      <form className={styles.form} onSubmit={submitHandler}>
+        <Input
+          type="email"
+          placeholder="Subscribe by email"
+          value={enteredEmail}
+          onChange={(event) => setEnteredEmail(event.target.value)}
+        />
         <Button type="submit" value="Send" />
       </form>
     </div>
