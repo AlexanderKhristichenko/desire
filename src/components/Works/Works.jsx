d2264265ebf82c5322b997154a337f7b7740ad77
Olexander Khrystychenko <alexander.khristichenko@gmail.com>
2023-10-30 21:54:19 +0200

    fixed optimization

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,45 +1,55 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./Works.module.scss";
 
 export const Works = () => {
+  const visibleContentBox = () => {
+    if (window.innerWidth > 768) {
+      return (
+        <div
+          className={`${styles["content-box"]} ${styles["visible-content-box"]}`}>
+          <div className={`${styles.box} ${styles.start}`}>
+            <div className={styles.header}>
+              <Image
+                src="/icons/works1.svg"
+                width={40}
+                height={40}
+                alt="works icon"
+              />
+              <h3>Designer Consultation</h3>
+            </div>
+            <p>
+              Kinfolk scenester authentic craft beer truffaut irony
+              intelligentsia YOLO lomo bushwick coloring book. Semiotics man bun
+              venmo viral cliche
+            </p>
+          </div>
+          <div className={styles.box}></div>
+          <div className={`${styles.box} ${styles.bottom}`}>
+            <div className={styles.header}>
+              <Image
+                src="/icons/works2.svg"
+                width={40}
+                height={40}
+                alt="works icon"
+              />
+              <h3>Production</h3>
+            </div>
+            <p>Everyday carry actually neutra authentic kogi shabby chic</p>
+          </div>
+          <div className={styles.box}></div>
+        </div>
+      );
+    }
+  };
+
   return (
     <section className={styles.works}>
       <div className="container">
         <h2 className={styles.title}>How it works</h2>
         <div className={styles.content}>
-          <div
-            className={`${styles["content-box"]} ${styles["visible-content-box"]}`}>
-            <div className={`${styles.box} ${styles.start}`}>
-              <div className={styles.header}>
-                <Image
-                  src="/icons/works1.svg"
-                  width={40}
-                  height={40}
-                  alt="works icon"
-                />
-                <h3>Designer Consultation</h3>
-              </div>
-              <p>
-                Kinfolk scenester authentic craft beer truffaut irony
-                intelligentsia YOLO lomo bushwick coloring book. Semiotics man
-                bun venmo viral cliche
-              </p>
-            </div>
-            <div className={styles.box}></div>
-            <div className={`${styles.box} ${styles.bottom}`}>
-              <div className={styles.header}>
-                <Image
-                  src="/icons/works2.svg"
-                  width={40}
-                  height={40}
-                  alt="works icon"
-                />
-                <h3>Production</h3>
-              </div>
-              <p>Everyday carry actually neutra authentic kogi shabby chic</p>
-            </div>
-            <div className={styles.box}></div>
-          </div>
+          {visibleContentBox()}
 
           <div className={styles.line}></div>
 
